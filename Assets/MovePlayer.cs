efc956950ca9d0e81b16514189f702301814fccf
Haroune Belhachani <2471001@cegepmv.ca>
2026-04-20 10:12:12 -0400

    update

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -8,23 +8,30 @@ public class MovePlayer : MonoBehaviour
     Rigidbody2D rb;
     public InputActionReference jumpAction;
     public float jumpSpeed = 10f;
-
     public InputActionReference mouse1;
     public InputActionReference point;
+    Animator anim;
+
+    string isRunningBool = "IsRunning";
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         
-        Vector3 dir = move.action.ReadValue<Vector2>();
+        Vector2 input = move.action.ReadValue<Vector2>();
 
-        if(Mathf.Abs(rb.linearVelocity.x) < 8)
-        {
-            rb.linearVelocity += new Vector2(dir.x, 0) * Time.deltaTime * moveSpeed;
-        }
+        //if(Mathf.Abs(rb.linearVelocity.x) < 8)
+        //{
+        //    rb.linearVelocity += new Vector2(input.x, 0) * Time.deltaTime * moveSpeed;
+        //}
+
+        transform.position += new Vector3(input.x, 0, 0) * Time.deltaTime * moveSpeed;
+
+        anim.SetBool(isRunningBool, input.magnitude > 0);
 
         if (jumpAction.action.WasPressedThisFrame() && Mathf.Abs(rb.linearVelocityY) < .1f)
         {
