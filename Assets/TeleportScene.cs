18d9bbbd51e8aa330719d076600bed290469a166
Haroune Belhachani <2471001@cegepmv.ca>
2026-05-04 12:19:01 -0400

    added mushroom fight, scene teleport

---
+++ Assets/TeleportScene.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportScene : MonoBehaviour
+{
+
+    public SceneAsset SceneAsset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async Task LoadNextScene()
+    {
+        await SceneManager.LoadSceneAsync(SceneAsset.name);
+    }
+
+    public async void OnDestroy()
+    {
+       await LoadNextScene();
+    }
+}
